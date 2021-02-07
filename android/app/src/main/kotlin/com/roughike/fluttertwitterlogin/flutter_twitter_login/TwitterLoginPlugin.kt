47ac9bc562a7659aab0accaed52c4e4622772904
Ricardo Sixel <sixel.femar@marinha.mil.br>
2021-02-07 15:42:51 -0300

    Fixing android`s project main class

--- android/app/src/main/kotlin/com/roughike/fluttertwitterlogin/flutter_twitter_login/TwitterLoginPlugin.kt
+++ android/app/src/main/kotlin/com/roughike/fluttertwitterlogin/flutter_twitter_login/TwitterLoginPlugin.kt
@@ -2,5 +2,5 @@ package com.roughike.fluttertwitterlogin.flutter_twitter_login
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class TwitterLoginPlugin: FlutterActivity() {
 }
